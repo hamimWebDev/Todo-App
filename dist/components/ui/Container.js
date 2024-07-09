3b84ab9fd482e6fa52039360c0222dc50af10ab1
hamimDev <mdhamimhowladerasif@gmail.com>
2024-07-09 07:45:54 +0600

    Hi

--- dist/components/ui/Container.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Container = ({ children }) => {
-    return <div className="h-screen w-full max-w-7xl mx-auto my-10">{children}</div>;
-};
-exports.default = Container;
